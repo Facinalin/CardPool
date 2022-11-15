298960de0fe7f47b15c258509cbaf7127626afec
facina <lovemistborn13@gmail.com>
2022-11-15 15:53:12 +0800

    1115demo

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -28,11 +28,31 @@ const positionElement = (e) =>{
   
   cursorBig.style.transform = `translate3d(${mouseX}px,${mouseY}px,0)`;
 }
-
 window.addEventListener('mousemove', positionElement);*/
 
-const head = document.querySelector('.member-row');
+const heads = document.querySelectorAll('.head');
+const headOt8 = document.querySelector('#head9');
+const memberImgAll = document.querySelectorAll('.per-member');
 
-head.addEventListener('clcik',(e) =>{
-  console.log(e.target.nodeName);
+heads.forEach((head) => {
+  head.addEventListener('click', (e) => {
+    
+    const arrow = e.target.nextElementSibling.children[1];
+    const memberImg = e.target.nextElementSibling.children[0];
+    if (e.target.checked && e.target.getAttribute('id')!='head9') {
+      arrow.classList.add('arr-active');
+      memberImg.classList.add('active');
+    } else if(e.target.getAttribute('id')==='head9'){
+      console.log('選到全員！');
+      memberImgAll.forEach(item=>{item.classList.remove('active')});
+      memberImg.classList.toggle('active'); 
+    }else{
+        arrow.classList.remove('arr-active');
+        memberImg.classList.remove('active');
+    }
+    console.log(e.target.getAttribute('id'));
+    console.log(memberImgAll);
+  })
 })
+
+
