03147d8381e4280600d4a88d6876770a1ed44bbb
facina <lovemistborn13@gmail.com>
2022-11-16 23:42:18 +0800

    Update 2022-11-16T15:42:16.333Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -41,18 +41,18 @@ heads.forEach(function (head) {
 
     if (headOt8.classList.contains('active') && e.target.getAttribute('id') != 'head9') {
       //點擊到的不是headOt8 且 headOt8已active時，清除headOt8的active
-      headOt8.classList.remove('active');
       console.log('選到個人！');
+      headOt8.classList.remove('active');
     } else if (e.target.getAttribute('id') === 'head9' && !e.target.classList.contains('active')) {
       //點擊到的是headOt8 且 點擊到的元素未包含active時，清除所有按鈕的active，再幫點到的該元素加上active
+      console.log('選到團體！');
       memberImgAll.forEach(function (item) {
         item.classList.remove('active');
       });
       e.target.classList.add('active');
-      console.log('選到團體！');
     } else {
-      e.target.classList.toggle('active');
       console.log('toggle！');
+      e.target.classList.toggle('active');
     }
 
     console.log(e.target.getAttribute('id'));
