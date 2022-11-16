11d1d6e5d0ade5fefd599521e7e10be0cc6716bb
facina <lovemistborn13@gmail.com>
2022-11-16 23:13:40 +0800

    1116-demo

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -40,15 +40,17 @@ heads.forEach((head) => {
     const arrow = e.target.nextElementSibling.children[1];
     const memberImg = e.target.nextElementSibling.children[0];
     if (headOt8.classList.contains('active') && e.target.getAttribute('id')!='head9') {
-      console.log('選到個人！'); //點擊到的不是headOt8 且 headOt8已active時，清除headOt8的active
+      //點擊到的不是headOt8 且 headOt8已active時，清除headOt8的active
       headOt8.classList.remove('active');
+      console.log('選到個人！');
     } else if(e.target.getAttribute('id')==='head9'&& !e.target.classList.contains('active')){
-      console.log('選到團體！'); //點擊到的是headOt8 且 點擊到的元素未包含active時，清除所有按鈕的active，再幫點到的該元素加上active
+       //點擊到的是headOt8 且 點擊到的元素未包含active時，清除所有按鈕的active，再幫點到的該元素加上active
       memberImgAll.forEach(item=>{item.classList.remove('active')});
       e.target.classList.add('active');
+      console.log('選到團體！');
     }else{
-        console.log('解除紫色');
         e.target.classList.toggle('active');
+        console.log('解除紫色');
     }
     console.log(e.target.getAttribute('id'));
   })
