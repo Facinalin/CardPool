bdd9edda760c5a9f0f17f18df8e1309953dd6261
facina <lovemistborn13@gmail.com>
2022-11-20 21:30:46 +0800

    1120-demo

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -33,6 +33,7 @@ window.addEventListener('mousemove', positionElement);*/
 const heads = document.querySelectorAll('.head');
 const headOt8 = document.querySelector('#head9');
 const memberImgAll = document.querySelectorAll('.per-member');
+    
 
 heads.forEach((head,i) => {
   head.addEventListener('click', (e) => {
@@ -42,22 +43,23 @@ heads.forEach((head,i) => {
     if (headOt8.classList.contains('active') && e.target.getAttribute('id')!='head9') {
       //點擊到的不是headOt8 且 headOt8已active時，清除headOt8的active
       console.log('選到個人！');
-      headOt8.classList.remove('active'); //<----這句沒反應
+      memberImgAll[8].classList.remove('active'); //<----第一次成功，第二次失敗（？？
       
     } 
-    
+    // ! debt here
     if(e.target.getAttribute('id')==='head9'&& !e.target.classList.contains('active')){
        //點擊到的是headOt8 且 點擊到的元素未包含active時，清除所有按鈕的active，再幫點到的該元素加上active
        console.log('選到團體變成紫！');
        memberImgAll.forEach(item=>{item.classList.remove('active')});
        memberImgAll[i].classList.toggle('active'); 
-       headOt8.classList.toggle('active');
+       headOt8.classList.toggle('active'); //memberImgAll[8].classList.toggle('active'); not working
     }
     else{
       console.log('toggle！');
       memberImgAll[i].classList.toggle('active');  
     }
     console.log(e.target.getAttribute('id'));
+    console.log(heads);
   })
 })
 
